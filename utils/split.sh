a5e4bd8fe773323b0f171b6625500576b1140103
Thierry Vinícius Lourenço de Lima <thierryv3011@gmail.com>
2023-09-18 19:06:42 +0000

    Ajustando split.sh pra separar por dobra

--- utils/split.sh
+++ utils/split.sh
@@ -11,7 +11,6 @@
 ############################################################
 Help() {
    # Display Help
-   #echo "A description of the script functions here:"
    echo "Syntax: ./splitFolds.sh [-s|S|h|H] [-k k_folds]"
    echo "options:"
    echo "h|H            Print this Help."
@@ -64,9 +63,9 @@ cp -R $dir_all/* $dir_train
 mkdir -p $dir_dobras/
 rm -rf $dir_dobras/*
 
-#realiza o agrupamento das imagens de mesmo individuo criando
-#uma pasta em cada uma das classes para cada um dos individuos
-#e movendo suas respectivas imagens para esta pasta
+# Realiza o agrupamento das imagens de mesmo indivíduo criando
+# uma pasta em cada uma das classes para cada um dos indivíduos
+# e movendo suas respectivas imagens para esta pasta
 if [[ $subset -eq 1 ]]; then
    for dir_class in $(ls $dir_train); 
    do
@@ -75,7 +74,7 @@ if [[ $subset -eq 1 ]]; then
       do
          string=$img"_"
          
-         #Split the text based on the delimiter
+         # Split the text based on the delimiter
          myarray=()
          while [[ $string ]]; do
             myarray+=( "${string%%"_"*}" )
@@ -93,7 +92,7 @@ if [[ $subset -eq 1 ]]; then
 fi
 
 for dir_class in $(ls $dir_train); do
-   echo "[SCRIPT SPLIT DATA] Spliting class -" $dir_class
+   echo "[SCRIPT SPLIT DATA] Splitting class -" $dir_class
    
    total_itens=$(ls $dir_train/$dir_class | wc -l)
    total_por_dobra_float=$(echo "scale=2; ($total_itens/$ndobras)" | bc -l)
@@ -113,7 +112,6 @@ for dir_class in $(ls $dir_train); do
       for file in $arrayFiles; do
          let "counter += 1"
          if [[ $counter -le $total_por_dobra ]]; then
-            # echo 'Count = ' $counter 'Moving ' $dir_train/$dir_class/$file ' to ' $dir_fold/$dir_class/$file
             mv $dir_train/$dir_class/$file $dir_fold/$dir_class/$file
          fi
       done
@@ -124,8 +122,8 @@ for dir_class in $(ls $dir_train); do
 
 done
 
-#reorganiza o conjunto de dados do diretório dir_dobras 
-#para ter apenas imagens e não subdiretórios dentro de cada uma das classes
+# Reorganiza o conjunto de dados do diretório dir_dobras 
+# para ter apenas imagens e não subdiretórios dentro de cada uma das classes
 if [[ $subset -eq 1 ]]; then
    for dir_fold in $(ls $dir_dobras); 
    do
@@ -133,8 +131,8 @@ if [[ $subset -eq 1 ]]; then
       do 
          for dir_item in $(ls ${dir_dobras}${dir_fold}"/"$dir_class);
          do 
-            #caso realmente existam diretórios de agrupamento das imagens 
-            #de determinado item dentro das classes de cada dobra
+            # Caso realmente existam diretórios de agrupamento das imagens 
+            # de determinado item dentro das classes de cada dobra
             if [ -d ${dir_dobras}${dir_fold}"/"$dir_class"/"$dir_item ]; then
                mv ${dir_dobras}${dir_fold}/${dir_class}/${dir_item}/* ${dir_dobras}${dir_fold}/${dir_class}/.
                rm -rf ${dir_dobras}${dir_fold}/$dir_class/$dir_item
@@ -145,4 +143,3 @@ if [[ $subset -eq 1 ]]; then
 fi
 
 echo "[SCRIPT SPLIT DATA] OK! DONE."
-
